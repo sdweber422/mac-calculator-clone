66277945bdf945301a0a713871f61153d71bc874
Stephan Weber <sdweber422@gmail.com>
2017-04-11 09:13:58 -0700

    Set up linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,33 @@
 module.exports = {
+    "env": {
+      "es6": false
+    },
     "extends": "standard",
     "plugins": [
         "standard",
         "promise"
-    ]
+    ],
+    "rules": {
+      "indent": [
+          "error",
+          2
+      ],
+      "semi" : [
+        "error",
+        "never"
+      ],
+      "space-in-parens": [
+        "error",
+        "always",
+        {
+          "exceptions": [
+            "[]", "{}", "()"
+          ]
+        }
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ]
+    }
 };
