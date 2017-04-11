de1e2cd38e529b94e0f85d5165ff9b0627c2c27a
Stephan Weber <sdweber422@gmail.com>
2017-04-11 14:31:15 -0700

    Connect front-end functions to Calculator class

--- controller.js
+++ controller.js
@@ -1,7 +1,50 @@
-var Calculator = require( './Calculator' )
-var clickHandler = document.getElementById( 'calculator-keypad' )
+var Calc = new Calculator() //eslint-disable-line
+var clickElement = document.querySelector( '.calculator-keypad' )
 
-clickHandler.addEventListener( '.click', function( event ) {
-  var value = event.target
-  console.log( event.target )
-} )
+clickElement.addEventListener( 'click', calculatorHandler.bind( this ) )
+document.addEventListener( 'keypress', calculatorHandler.bind( this ) )
+
+function calculatorHandler ( event ) {
+  var displayElement = document.querySelector( '.calculator-display' )
+  var value = ( !event.key ) ? event.target.value : parsedKey( event.key )
+
+  Calc.acceptInput( value )
+  displayResizer( displayElement )
+  checkForMaxDisplay( displayElement )
+}
+
+function checkForMaxDisplay ( display ) {
+  if ( Calc.getDisplayLength() < 24 ) {
+    display.innerHTML = Calc.getDisplay()
+  }
+}
+
+function displayResizer ( display ) {
+  var displayLength = Calc.getDisplayLength()
+  if ( displayLength > 8 ) {
+    var fontSize = window.getComputedStyle( display, null )
+      .getPropertyValue( 'font-size' )
+    fontSize = fontSize.slice( 0, fontSize.length - 2 )
+    var alteredFontSize = fontSize - ( fontSize / 10 )
+    if ( alteredFontSize > 20 ) {
+      display.style.fontSize = `${alteredFontSize}px`
+    }
+  }
+}
+
+function parsedKey ( value ) {
+  if ( value.charCodeAt() > 44 && value.charCodeAt() < 58 ) {
+    return value
+  } else {
+    switch ( value.charCodeAt() ) {
+      case 127: return 'AC'
+      case 69 : return '='
+      case 42 :
+      case 88 :
+      case 120: return 'X'
+      case 37 : return '%'
+      case 43 : return '+'
+      default : return null
+    }
+  }
+}
