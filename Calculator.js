b9dcd1f5eefae6bc239f6486bea00b5855e5fe21
Stephan Weber <sdweber422@gmail.com>
2017-04-11 16:33:57 -0700

    WIP:Add comments to organize, add simple functionality with calc functions

--- Calculator.js
+++ Calculator.js
@@ -2,9 +2,12 @@ var Calculator = function () {
   this.operations = []
   this.operands = []
   this.display = '0'
+  this.displayFlag = true
 }
 
 Calculator.prototype = {
+
+  // Functions which interact with external interface
 
   acceptInput: function ( value ) {
     if ( value === null ) {
@@ -14,13 +17,37 @@ Calculator.prototype = {
     this.handleInput()
   },
 
+  displayScreen: function () {
+    this.displayFlag = true
+    if ( this.display === '' ) {
+      this.display = '0'
+    } else if ( this.display === '0' ) {
+      this.display = this.value
+    } else {
+      this.display += this.value
+    }
+  },
+
+  getDisplay: function () {
+    return this.display
+  },
+
+  getDisplayLength: function () {
+    return this.display.length
+  },
+
+  shouldDisplay: function () {
+    return this.displayFlag
+  },
+
+  // Special operation functions
+
   allClear: function () {
     this.operands = []
     this.operations = []
     this.display = '0'
+    this.displayFlag = true
     this.value = '0'
-    console.log( 'this.display', this.display )
-    console.log( 'this.display.length', this.display.length )
     return this.displayScreen()
   },
 
@@ -37,6 +64,65 @@ Calculator.prototype = {
     this.value = ''
     return this.displayScreen()
   },
+
+  completeOperations: function () {
+    while ( this.operations.length ) {
+      this.display = this.lookUpFunctions(
+        this.operations.pop(),
+        this.display,
+        this.operands.pop()
+      )
+    }
+    this.value = ''
+    this.displayFlag = true
+    return this.displayScreen()
+  },
+
+  // Functions that control calculator based on input
+
+  handleInput: function () {
+    if ( this.isNumber() ) {
+      this.displayScreen()
+      return
+    }
+    if ( this.isOperation() ) {
+      this.handleOperations()
+      return
+    }
+    return this.handleSpecialOperations()
+  },
+
+  handleOperations: function () {
+    if ( this.isEqualSign() ) {
+      return this.completeOperations()
+    }
+    if ( !this.queuedOperations() ) {
+      this.operations.push( this.value )
+      this.operands.push( this.display )
+      this.displayFlag = false
+      this.display = '0'
+    }
+  },
+
+  handleSpecialOperations: function () {
+    if ( this.checkForAllClear() ) {
+      return this.allClear()
+    }
+    if ( this.checkForDecimal() ) {
+      if ( !this.displayContainsDecimal() ) {
+        this.displayScreen()
+      }
+      return
+    }
+    if ( this.checkForSign() ) {
+      return this.changeSign()
+    }
+    if ( this.checkForPercentage() ) {
+      this.computePercentage()
+    }
+  },
+
+  // Boolean functions
 
   checkForAllClear: function () {
     return this.value === 'AC'
@@ -62,53 +148,8 @@ Calculator.prototype = {
     return this.display.includes( '.' )
   },
 
-  displayScreen: function () {
-    if ( this.display === '0' ) {
-      this.display = this.value
-    } else {
-      this.display += this.value
-    }
-  },
-
-  getDisplay: function () {
-    return this.display
-  },
-
-  getDisplayLength: function () {
-    return this.display.length
-  },
-
-  handleInput: function () {
-    if ( this.isNumber() ) {
-      this.displayScreen()
-      return
-    }
-    if ( this.isOperation() ) {
-      this.handleOperations()
-      return
-    }
-    return this.handleSpecialOperations()
-  },
-
-  handleOperations: function () {
-  },
-
-  handleSpecialOperations: function () {
-    if ( this.checkForAllClear() ) {
-      return this.allClear()
-    }
-    if ( this.checkForDecimal() ) {
-      if ( !this.displayContainsDecimal() ) {
-        this.displayScreen()
-      }
-      return
-    }
-    if ( this.checkForSign() ) {
-      return this.changeSign()
-    }
-    if ( this.checkForPercentage() ) {
-      this.computePercentage()
-    }
+  isEqualSign: function () {
+    return this.value === '='
   },
 
   isNumber: function () {
@@ -117,5 +158,27 @@ Calculator.prototype = {
 
   isOperation: function () {
     return [ '=', 'X', '/', '+', '-' ].includes( this.value )
+  },
+
+  queuedOperations: function () {
+    return this.operations.length !== 0
+  },
+
+  // Look up table for functions
+
+  lookUpFunctions: function ( operator, operandOne, operandTwo ) {
+    var firstValue = parseFloat( operandOne )
+    var secondValue = parseFloat( operandTwo )
+    /* eslint-disable key-spacing */
+    var functions = {
+      'X'        : firstValue * secondValue,
+      '/'        : firstValue / secondValue,
+      '+'        : firstValue + secondValue,
+      '-'        : firstValue - secondValue,
+      'null'     : '0',
+      'undefined': '0'
+    }
+    /* eslint-enable */
+    return functions[ operator ] || '0'
   }
 }
